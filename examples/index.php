8aaafc98b24615207d7f5427a1a5176921d3890f
Kévin Bibollet <bibollet.kevin@gmail.com>
2017-12-14 00:47:57 +0100

    Add files via upload

--- examples/index.php
+++ examples/index.php
@@ -11,30 +11,30 @@ foreach (Autoload::load('../src/') as $file)
 }
 /* End autoloading */
 
-use iArcadia\MagicPokeAPI\API;
+use iArcadia\MagicPokeAPI\PokeAPI;
 
 /*
  * We create a new instance of the PokeAPI wrapper.
  */
-$api = new API();
+$api = new PokeAPI();
 
 /* Gets from PokeAPI server, data of the pokemon named "Arcanine". */
-$arcanine = $api->resource(API::RESOURCE_POKEMON)->find('arcanine');
+$arcanine = $api->resource(PokeAPI::RESOURCE_POKEMON)->find('arcanine');
 //var_dump($arcanine);
 
 /* Gets from PokeAPI server, data of 5 abilities, starting from the 10th. */
-$abilities = $api->resource(API::RESOURCE_ABILITY)->limit(5)->offset(10)->get();
+$abilities = $api->resource(PokeAPI::RESOURCE_ABILITY)->limit(5)->offset(10)->get();
 //var_dump($abilities);
 
 /* Gets from PokeAPI server, data of 22 berries, starting from the 3rd. */
-$api->resource(API::RESOURCE_BERRY);
-$api->offset(3);
+$api->resource(PokeAPI::RESOURCE_BERRY);
+$api->skip(3);
 $api->limit(22);
 $berries = $api->get();
 //var_dump($berries);
 
 /* Gets from PokeAPI server, data of the region of ID 4. */
-$api->resource(API::RESOURCE_REGION);
+$api->resource(PokeAPI::RESOURCE_REGION);
 $region = $api->find(4);
 //var_dump($region);
 
@@ -45,3 +45,12 @@ $api->setCaching(false);
 $api->cacheExpiration(60 * 60 * 24* 30);
 
 //var_dump($api);
+
+$t = new PokeAPI(
+[
+    'limit' => 6,
+    'offset' => 9,
+    'resource' => PokeAPI::RESOURCE_LOCATION
+]);
+
+var_dump($t);
