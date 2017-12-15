3269949060aab02f2608d77b975f9b147db49221
iArcadia <bibollet.kevin@gmail.com>
2017-12-15 15:02:21 +0100

    Commit for v1.1.0

---
+++ src/config/lang.php
@@ -0,0 +1,34 @@
+<?php
+
+return
+[
+    /*
+     * Do you want to use the automatic resource name translation?
+     *
+     * Value type: boolean
+     * - true
+     * - false
+     */
+    'use' => true,
+    
+    /*
+     * Which language do you want to use?
+     *
+     * !!!!
+     * CURRENTLY ONLY WORK FOR POKEMON NAMES
+     * English names will always work.
+     * Spanish and italian names are the same as the english ones.
+     * !!!!
+     *
+     * Value type: string
+     * - de
+     * - fr
+     * - ja
+     * - ja-Hrkt
+     * - ko
+     * - roomaji
+     * - zh-Hans
+     * - zh-Hant
+     */
+    'lang' => 'fr',
+];
