6fb4b1bc8f8e2e642c247a6ff2ea7feb30f872b8
iArcadia <bibollet.kevin@gmail.com>
2017-12-15 17:20:28 +0100

    Create src/Helpers/URLBuilder.php

---
+++ src/Helpers/URLBuilder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace iArcadia\MagicPokeAPI\Helpers;
+
+/**
+ * Constructs API URLs.
+ *
+ * @author Kévin Bibollet <bibollet.kevin@gmail.com>
+ * @license MIT
+ */
+class URLBuilder
+{
+    
+}
